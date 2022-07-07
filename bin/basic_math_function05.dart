bf24cd38e4843dd417ddd948871c65c04fd70137
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-07 11:57:09 +0500

    cos

--- bin/basic_math_function05.dart
+++ bin/basic_math_function05.dart
@@ -8,9 +8,9 @@ Create function named func with a argument
       result (float): multiply x by the cosine of y. Round the result.
 */
 import "dart:math";
-double func(int x,double y){
+double func(int x,int y){
     double m=x*(cos(y));
-    
+
     int s=m.round();
     return s;
 }
