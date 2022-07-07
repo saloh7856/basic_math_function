ac5d3c86b7caefe6bee43ef281aa55b6915b44e0
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-07 11:40:16 +0500

    int

--- bin/basic_math_function04.dart
+++ bin/basic_math_function04.dart
@@ -3,12 +3,12 @@
     Multiply one number by the sine of another number.
     Args:
         y  (int): radian number
-        x (int): multiply x by the sine of y
+        x (double): multiply x by the sine of y
     Returns:
         result (double): multiply x by the sine of y. Round the result.
 */
 import 'dart:math';
-int func(int x,int y){
+int func(int x, double int y){
     double  m=x*(sin(y));
     int s = m.round();
     return s;
